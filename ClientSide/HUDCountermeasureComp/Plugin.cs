c62d4ff1045a229562eb7cb21da9a6bc61fa93f1
Assassin1076 <assassin1076@163.com>
2026-06-09 19:13:52 +0800

    上传干扰措施HUD显示组件Mod

---
+++ ClientSide/HUDCountermeasureComp/Plugin.cs
@@ -0,0 +1,176 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HUDCountermeasureComp
+{
+    public static class HUDCountermeasureCompInfo
+    {
+        public const string PLUGIN_GUID = "HUDCountermeasureComp";
+        public const string PLUGIN_NAME = "Countermeasure HUD";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+
+
+    [BepInPlugin(HUDCountermeasureCompInfo.PLUGIN_GUID, HUDCountermeasureCompInfo.PLUGIN_NAME, HUDCountermeasureCompInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        public bool done = false;
+        internal static ConfigEntry<float> HUDX;
+        internal static ConfigEntry<float> HUDY;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {HUDCountermeasureCompInfo.PLUGIN_GUID} is loaded!");
+            HUDX = Config.Bind(
+                "HUD",
+                "PositionX",
+                -260f,
+                "HUD X Position");
+
+            HUDY = Config.Bind(
+                "HUD",
+                "PositionY",
+                -140f,
+                "HUD Y Position");
+
+        }
+        void Update()
+        {
+            if (done) return;
+            if (Encyclopedia.i == null)
+            {
+                Logger.LogInfo("Waiting for target prefabs to load...");
+                return;
+            }
+            foreach (AircraftDefinition aircraft in Encyclopedia.i.aircraft)
+            {
+                testFunc(aircraft, aircraft.aircraftParameters.HUDExtras.transform);
+                //要获取Aircraft类，通过aircraft.unitPrefab.GetComponent<Aircraft>即可
+            }
+
+            done = true;
+        }
+
+        public void testFunc(AircraftDefinition aircraftDef, Transform root)
+        {
+            Aircraft aircraft = aircraftDef.unitPrefab.GetComponent<Aircraft>();
+
+            if (aircraft == null)
+                return;
+
+            if (root.Find("CountermeasureIndicator_Assassin1076") != null && root.Find("CountermeasureIndicator_Assassin1076").gameObject.GetComponent<CountermeasureIndicator>() != null)
+            {
+                Logger.LogInfo(
+                    $"Countermeasure HUD already exists for {aircraftDef.name}, skipping...");
+                return;
+            }
+            GameObject hudObj = new GameObject("CountermeasureIndicator_Assassin1076");
+            hudObj.transform.SetParent(root, false);
+
+            CountermeasuresIndicator indicator =
+                hudObj.AddComponent<CountermeasuresIndicator>();
+
+            Logger.LogInfo(
+                $"Added Countermeasure HUD to {aircraftDef.name}");
+        }
+    }
+    public class CountermeasuresIndicator : MonoBehaviour
+    {
+        public Aircraft host = null;
+
+        private Text counterText;
+        private RectTransform rect;
+
+        void Awake()
+        {
+            rect = gameObject.AddComponent<RectTransform>();
+
+            rect.anchorMin = new Vector2(1, 1);
+            rect.anchorMax = new Vector2(1, 1);
+            rect.pivot = new Vector2(1, 1);
+
+            rect.anchoredPosition =
+                new Vector2(
+                    Plugin.HUDX.Value,
+                    Plugin.HUDY.Value);
+
+            GameObject txtObj = new GameObject("Text");
+            txtObj.transform.SetParent(transform, false);
+
+            counterText = txtObj.AddComponent<Text>();
+
+            counterText.font =
+                Resources.GetBuiltinResource<Font>("Arial.ttf");
+
+            counterText.fontSize = 18;
+            counterText.alignment = TextAnchor.UpperRight;
+
+            counterText.horizontalOverflow =
+                HorizontalWrapMode.Overflow;
+
+            counterText.verticalOverflow =
+                VerticalWrapMode.Overflow;
+
+            counterText.color = Color.green;
+            counterText.supportRichText = true;
+            RectTransform txtRect =
+                txtObj.GetComponent<RectTransform>();
+
+            txtRect.anchorMin = Vector2.zero;
+            txtRect.anchorMax = Vector2.one;
+            txtRect.offsetMin = Vector2.zero;
+            txtRect.offsetMax = Vector2.zero;
+        }
+
+        void Update()
+        {
+            if (host == null) {
+                host = SceneSingleton<CombatHUD>.i.aircraft;
+            }
+
+            if (host.countermeasureManager == null)
+                return;
+
+            var stations =
+                host.countermeasureManager.countermeasureStations;
+
+            if (stations == null)
+                return;
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CM");
+
+            float charge = 0f;
+
+            try
+            {
+                charge =
+                    host.GetPowerSupply().GetCharge();
+            }
+            catch
+            {
+            }
+
+            foreach (var station in stations)
+            {
+                if (station == null)
+                    continue;
+
+                string name = station.displayName;
+                sb.AppendLine(
+                        $"{name} {station.ammo}");
+            }
+            sb.AppendLine(
+                        $"PowerCharge {charge * 100f:F0}%");
+
+            counterText.text = sb.ToString();
+        }
+    }
+}
